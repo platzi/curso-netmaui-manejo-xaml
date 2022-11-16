bf20e7f7009ac8e4bedda1048a9826b39b195ad9
LeomarisReyes <reyes.leomaris@gmail.com>
2022-11-16 12:15:21 -0400

    Splash screen in .NET MAUI

---
+++ 1.Splash_Screen/PlatziMauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PlatziMauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
