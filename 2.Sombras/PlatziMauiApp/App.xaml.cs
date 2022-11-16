5b9b9972a8ff65ebbf57277ffe2042e357635ac3
LeomarisReyes <reyes.leomaris@gmail.com>
2022-11-16 12:17:11 -0400

    Sombras en .NET MAUI

---
+++ 2.Sombras/PlatziMauiApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PlatziMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+         
+        MainPage = new Views.ExplorandoFeatures.SombraSamplePage();
+
+    }
+}
+
